26e1b50095a006192a597b625b160464968743d1
SatMunia <SatMunia@192.168.56.1>
2017-11-24 00:21:32 +0530

    springweb

---
+++ spring/src/main/java/spring/Helloworld.java
@@ -0,0 +1,21 @@
+package spring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Helloworld {
+	@RequestMapping("/")
+    String hello() {
+        return "Hello World! sathish";
+    }
+	public static void main(String[] args) {
+		 SpringApplication.run(Helloworld.class, args);
+
+	}
+
+}
